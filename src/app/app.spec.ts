f7db978d78fd52406d2ba7cf17e43201b1b87187
Lombardo Gonzalez G <lombardo.gonzalez@flyr.com>
2025-11-26 22:01:39 -0500

    Revert "chore: Eliminar archivos no utilizados (LEEME.md y archivos spec)"
    This reverts commit d91285174cbdf123ae671de93e23f99cf7a17e0b.

---
+++ src/app/app.spec.ts
@@ -0,0 +1,25 @@
+import { TestBed } from '@angular/core/testing';
+import { App } from './app';
+import { provideMockStore } from '@ngrx/store/testing';
+
+describe('App', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [App],
+      providers: [provideMockStore({})]
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should render footer', async () => {
+    const fixture = TestBed.createComponent(App);
+    await fixture.whenStable();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.app-footer')).toBeTruthy();
+  });
+});
